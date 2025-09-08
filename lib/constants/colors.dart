75a71ad694dabaf1a98091c9d8885a446d6bfa37
diazkr <juliethdiaz001@gmail.com>
2025-09-07 19:34:57 -0500

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static const Color primaryColor = Color(0xFF7C3AED);
+  static const Color secondaryColor = Color(0xFF22D3EE);
+  static const Color accentColor = Color(0xFFFFFFFF);
+}
---
+++ lib/screens/game.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../constants/colors.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  bool oTurn = true;
+  List<String> displayXO = ['', '', '', '', '', '', '', '', ''];
+
+  static var customFontWhite = GoogleFonts.coiny(
+    textStyle: const TextStyle(
+      color: Colors.white,
+      letterSpacing: 3,
+      fontSize: 28,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MainColor.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(flex: 1, child: Center(child: Text('Score Board'))),
+            Expanded(
+              flex: 3,
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      _tapped(index);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          width: 5,
+                          color: MainColor.primaryColor,
+                        ),
+                        color: MainColor.secondaryColor,
+                      ),
+                      child: Center(
+                        child: Text(
+                          displayXO[index],
+                          style: GoogleFonts.coiny(
+                            textStyle: TextStyle(
+                              fontSize: 64,
+                              color: MainColor.primaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(flex: 2, child: Text('Timer')),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (oTurn && displayXO[index] == '') {
+        displayXO[index] = 'O';
+      } else {
+        displayXO[index] = 'X';
+      }
+
+      oTurn = !oTurn;
+    });
+  }
+}
